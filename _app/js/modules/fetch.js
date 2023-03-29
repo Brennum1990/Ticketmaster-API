ba953019710da2f02c6775f5d088535412a2af40
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2023-03-29 13:55:53 +0200

    Added changes to fetch java

--- _app/js/modules/fetch.js
+++ _app/js/modules/fetch.js
@@ -1,6 +1,6 @@
 export default async function fetchEvents() {
     const baseUrl = 'https://app.ticketmaster.com/discovery/v2/';
-    const apiKey = 'XvKLJTgjurq3X2HHq16a8PAAGOYs6Dah&size=200';
+    const apiKey = 'XvKLJTgjurq3X2HHq16a8PAAGOYs6Dah';
     const options = {
         method: 'GET'
     };
@@ -8,7 +8,7 @@ export default async function fetchEvents() {
     fetch(`${baseUrl}events?apikey=${apiKey}`, options)
         .then((response) => response.json())
         .then((json) => console.log(json))
-
+        
         const eventsList = document.getElementById('eventsList');
         const searchBar = document.getElementById('searchBar');
         let ticketMasterEvents = [];
@@ -35,36 +35,31 @@ export default async function fetchEvents() {
                 console.error(err);
             }
         };
+        loadEvents();
 
-        // const displayEvents = (events) => {
-        //     const htmlString = events
-        //         .map((event) => {
-        //             return `
-        //             <li class="event">
-        //                 <h2>${event.name}</h2>
-        //                 <p>House: ${event.url}</p>
-        //                 <img src="${event.image}"></img>
-        //             </li>
-        //         `;
-        //         })
-        //         .join('');
-        //     eventsList.innerHTML = htmlString;
-        // };
+        // event.forEach(events => {
+        //     const liElement = document.createElement('li');
+        //     liElement.textContent = `
+        //         ${events.name},
+        //         ${events.address}, 
+        //         ${events.url},
+        //         ${events.dates}`;
+        //     eventsList.appendChild(liElement);
+        // })
 
-        // loadEvents();
-        
-
-    // const response = await fetch(endopoint, options);       
-
-    // const eventsList = document.getElementById('eventsList');
-    // const searchBar = document.getElementById('searchBar');
-
-    // Event.forEach(events => {
-    //     const liElement = document.createElement('li');
-    //     liElement.textContent = `
-    //         ${events.name} - 
-    //         ${events.url}`;
-    //     eventsList.appendChild(searchBar);
-    // })
+        const displayEvents = (event) => {
+            const htmlString = event
+                .map((events) => {
+                    return `
+                    <li class="events">
+                        <h2>${events.name}</h2>
+                        <p>${events.url}</p>
+                        <p>${events.dates}></p>
+                    </li>
+                `;
+                })
+                .join('');
+            eventsList.innerHTML = htmlString;
+        };
 
 }

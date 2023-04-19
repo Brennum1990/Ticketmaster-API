75bd7c9e02209547b545610eb99222f0106c147c
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2023-04-19 02:56:25 +0200

    Adding comments & fixing bugs

--- _app/js/modules/fetchEvents.js
+++ _app/js/modules/fetchEvents.js
@@ -1,16 +1,15 @@
 import { apiKey } from "../env.js";
 
 export default async function fetchEvents() {
-    const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events?';
+    const baseUrl = 'https://app.ticketmaster.com/discovery/v2';
     const size = '200';
     const options = {
         method: 'GET',
     }; 
 
     // Fetching API
-    const endpoint = `${baseUrl}apikey=${apiKey}&size=${size}`;
+    const endpoint = `${baseUrl}/events?apikey=${apiKey}&size=${size}`;
     const response = await fetch(endpoint, options)
-        console.log(response);
         try {
             const ticketMasterEvents = await handleResponse(response);
             return ticketMasterEvents;
@@ -19,11 +18,13 @@ export default async function fetchEvents() {
             handleError(error);
         }
     
-    // Response - if ok = result || if not ok = Error
+    // Response - if ok = result
     async function handleResponse(response) {
         if(response.ok) {
             const result = await response.json();
-                console.log(result);
+                console.log(result)
+
+    // Response - if not ok = Error         
             } else if (response.status === 404) {
                 throw new Error('Url not existing');
             } else if (response.status === 401) {
@@ -40,25 +41,40 @@ export default async function fetchEvents() {
         warningElement.textContent = error;
     }
 
+        // const eventName = document.createElement('h2');
+
+        // eventName.className = 'event__name';
+
+        // AllEvents.forEach(events => {
+        //     const liElement = document.createElement('li');
+        //     liElement.textContent = `
+        //     <h1>${_embedded.events.name}</h1>
+        //     `;
+        // // eventContainer.insertAdjacentHTML('afterbegin', html);
+        // ulElement.appendChild(liElement);
+        // })
+   
+
     // Rendering eventitems to HTML
-    const eventList = document.getElementById('eventList');
-    result(ticketMasterEvents);
-    let ticketMasterEvents = [];
+    // const eventList = document.getElementById('eventList');
+    // document.querySelector('eventList').innerText = result
+    // result(ticketMasterEvents);
+    // let ticketMasterEvents = [];
     
-    const renderEvents = (allEvents) => {
-        const htmlString = allEvents
-            .map((events) => {
-                return `
-                    <li class="events">
-                    <h1>${events.name}</h1>
-                    <img src="${events.images}"></img>
-                    <p>${events.type}</p>
-                    <p>${events.dates}</p>
-                    <p>${events._embedded.venues}</p>
-                    </li>`;
-                })
-                .join('');
-            eventList.innerHTML = htmlString;
-        };
-        renderEvents();
+    // const renderEvents = (allEvents) => {
+    //     const htmlString = allEvents
+    //         .map((events) => {
+    //             return `
+    //                 <li class="events">
+    //                 <h1>${events.name}</h1>
+    //                 <img src="${events.images}"></img>
+    //                 <p>${events.type}</p>
+    //                 <p>${events.dates}</p>
+    //                 <p>${events._embedded.venues}</p>
+    //                 </li>`;
+    //             })
+    //             .join('');
+    //         eventList.innerHTML = htmlString;
+    //     };
+    //     renderEvents();
 }

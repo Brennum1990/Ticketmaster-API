ecdcff98107f663b91597cb44bfaaef3380551a7
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2023-04-21 01:12:22 +0200

    Added render events function

--- _app/js/modules/displayEvents.js
+++ _app/js/modules/displayEvents.js
@@ -1,16 +1,36 @@
-export default async function displayEvents() {
-        const eventContainer = document.querySelector('.events__container');
-        eventContainer.innerHTML = [];
-        
-        const eventList = result._embedded.events;
-        eventList.forEach(event => {
-            const html = event
-            .map((events) => {
-                return `
-                <h1 class="event__name">${events.name}</h1>
-                `;
-            })
-            .join('');
-            eventList.innerHTML = html;
-        })
+export default async function displayEvents(event) {
+    const eventContainer = document.querySelector('.events');
+    
+    // Creating elements
+    const eventCard = document.createElement('div');
+    const eventImageFrame = document.createElement('div');
+    const eventInfo = document.createElement('div');
+    const eventName = document.createElement('h1');
+    const eventImage = document.createElement('img');
+    const eventVenue = document.createElement('h3');
+    const eventCity = document.createElement('h3');
+
+    // Rendering elements 
+    eventName.innerHTML = event.name; 
+    eventImage.innerHTML = event.images[0];
+    eventVenue.innerText = event._embedded.venues[0].name;
+    eventCity.innerText = event._embedded.venues[0].city.name; 
+
+    // Hierarchy of event info
+    eventContainer.appendChild(eventCard);
+        eventCard.appendChild(eventImageFrame);
+            eventImageFrame.appendChild(eventImage);
+        eventCard.appendChild(eventInfo);
+            eventInfo.appendChild(eventName);
+            eventInfo.appendChild(eventVenue);
+            eventInfo.appendChild(eventCity);
+    
+    // Generating classname
+    eventCard.className = 'event__card';
+    eventImageFrame.classname = 'event__image-frame';
+    eventImage.classname = 'event__image';
+    eventInfo.className = 'event__info'; 
+    eventName.className = 'event__name';
+    eventVenue.classname = 'event__venue';
+    eventCity.classname = 'event__city';
 }

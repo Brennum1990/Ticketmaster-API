0d4bd29edc6ab1e2b16728850f14759de1539326
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2023-04-26 14:32:47 +0200

    Added filter to searchbar

---
+++ _app/js/modules/filterEvents.js
@@ -0,0 +1,37 @@
+import fetchEvents from "./fetchEvents.js";
+
+export default async function filterEvents() {
+
+	const eventsContainer = document.querySelector('.events');
+	
+	let filteredEvents = [];
+	const searchInput = document.getElementById('searchBar');
+		console.log(searchBar);
+	searchInput.addEventListener('keyup', e => {
+		const searchString = e.target.value; 
+		if(searchString.length > 2) {
+			filteredEvents.filter(events => {
+				events.name.contains(searchString) ||
+				events._embedded.venues[0].name.contains(searchString);
+			});
+			eventsContainer.innerText = '';
+			fetchEvents('search', searchString);
+		}
+	})
+
+	const searchButton = document.getElementById('submitBtn');
+	searchButton.addEventListener('click', handleSearchButton);
+	
+	function handleSearchButton() {
+		let buttons = document.querySelector('Search');
+			buttons.forEach((button) => {
+		if (value.toLowercase() == button.innerText.toLowercase()) {
+			button.classList.add('active');
+			}
+		else {
+			button.classList.remove('active'); 
+			}
+		})
+	}
+}
+
